4825b74fe5649bc25c5d5c924aaccbfe68ac4a8e
Brendon McKinley <brendon.mckinley@gmail.com>
2016-03-05 12:09:30 +1100

    Initial Commit

---
+++ ForteTeeVee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ForteTeeVee
+//
+//  Created by brendon mckinley on 5/03/2016.
+//  Copyright © 2016 Brendon McKinley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
